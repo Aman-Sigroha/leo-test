fa0b70ac334618cf4eb4d3ded48a2b0fdca267fc
Aman-Sigroha <amansigroha4@gmail.com>
2025-09-05 16:58:50 +0530

    Fix React Router compatibility and TypeScript errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OnshapeAuth from './components/OnshapeAuth';
 import AuthCallback from './components/AuthCallback';
